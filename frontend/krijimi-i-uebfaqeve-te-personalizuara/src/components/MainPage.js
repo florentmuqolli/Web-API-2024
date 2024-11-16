fabfeece5464bda5f45537670fcc35ab217164c4
florentmuqolli <149383933+florentmuqolli@users.noreply.github.com>
2024-11-16 23:58:55 +0100

    mysql added

--- frontend/krijimi-i-uebfaqeve-te-personalizuara/src/components/MainPage.js
+++ frontend/krijimi-i-uebfaqeve-te-personalizuara/src/components/MainPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MainPage = () => {
     return (
         <div className="text-center text-light" style={{ paddingTop: '20%', fontSize: '1.5rem'}}>
-            <h1>Welcome to Ueb Templates</h1>
+            <h1>Welcome to Web Templates</h1>
             <p>Explore our selection of templates for your projects.</p>
         </div>
     );
